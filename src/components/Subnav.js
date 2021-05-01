379e2c176f66e4a71b6bc91a047da089626819a5
bmuhamood <bbosa2009@gmail.com>
2021-05-01 10:20:28 -0700

    added Timeline.js and modified Subnav.js bgcolor

--- src/components/Subnav.js
+++ src/components/Subnav.js
@@ -4,7 +4,7 @@ import {Button } from "react-bootstrap";
 
 function subnav() {
     return (
-      <div class="mt-2 bg-light text-left ml-2">
+      <div class="mt-2 text-left ml-2">
         <>
           <Button variant="warning">New</Button>{" "}
           <Button variant="secondary" class="bg-info text-gray">Past</Button>{" "}
