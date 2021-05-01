005380684dc0aa1148ba8eb65d985a961f200c50
bmuhamood <bbosa2009@gmail.com>
2021-05-01 10:26:35 -0700

    fixed layout issues on Timeline.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Subnav from './components/Subnav';
+import Timeline from './components/Timeline';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Subnav />
+      <Timeline />
+      <Timeline />
+      <Timeline />
     </div>
   );
 }
